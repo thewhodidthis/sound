c13497ff9b2f3fcbfa8934c6bd5c679d7a9edae1
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-15 19:21:06 +0200

    module: bring back cjs version

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
+'use strict';
+
 // # Sound
 // Helps make baudio style monophonic musics
 
 const createSignal = (settings = {}, callback = (() => {})) => {
-  const { bufferSize = 512, context = new AudioContext() } = settings
-  const crunch = typeof settings === 'function' ? settings : callback
+  const { bufferSize = 512, context = new AudioContext() } = settings;
+  const crunch = typeof settings === 'function' ? settings : callback;
 
-  const worker = context.createScriptProcessor(bufferSize, 1, 1)
-  const stride = 1 / context.sampleRate
+  const worker = context.createScriptProcessor(bufferSize, 1, 1);
+  const stride = 1 / context.sampleRate;
 
-  let tick = 0
+  let tick = 0;
 
   worker.onaudioprocess = ({ outputBuffer: outgoing, inputBuffer: incoming }) => {
-    const prev = incoming.getChannelData(0)
-    const next = outgoing.getChannelData(0)
-    const stop = prev.length
+    const prev = incoming.getChannelData(0);
+    const next = outgoing.getChannelData(0);
+    const stop = prev.length;
 
     for (let i = 0; i < stop; i += 1) {
-      next[i] = crunch(tick * stride, i, prev[i])
+      next[i] = crunch(tick * stride, i, prev[i]);
 
-      tick += 1
+      tick += 1;
     }
-  }
+  };
 
   return worker
-}
+};
 
-export default createSignal
+module.exports = createSignal;
