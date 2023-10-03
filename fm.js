eb2d23551be180cd8210fd64f6a2b4b3f7561944
thewhodidthis <thewhodidthis@fastmail.com>
2023-10-03 01:40:38 +0100

    js/fm: use constructors instead of factory methods

--- fm.js
+++ fm.js
@@ -2,13 +2,13 @@
 export function fm(target) {
   const { context } = target
   const carrier = {
-    vco: context.createOscillator(),
-    vca: context.createGain(),
+    vco: new OscillatorNode(context),
+    vca: new GainNode(context),
   }
 
   const modulator = {
-    vco: context.createOscillator(),
-    vca: context.createGain(),
+    vco: new OscillatorNode(context),
+    vca: new GainNode(context),
   }
 
   return function voice(frequency = 440, ratio = 1, depth = 80) {
